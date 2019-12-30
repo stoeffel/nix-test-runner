8faa5c6b14204a408a7fdf11fe6bae20a1f99cfc
Stoeffel <schtoeffel@gmail.com>
2019-12-30 13:04:36 +0100

    nicer output

--- examples/failing.nix
+++ examples/failing.nix
@@ -7,4 +7,18 @@
     expr = builtins.add 1 1;
     expected = 2;
   };
+  testFailed2 = {
+    expr = {
+      a = 1;
+      b = 2;
+    };
+    expected = {
+      a = 1;
+      b = 1;
+    };
+  };
+  testFailed3 = {
+    expr = [ 1 2 3 ];
+    expected = [ 1 2 4 5 ];
+  };
 }
--- src/runTest.nix
+++ src/runTest.nix
@@ -5,10 +5,33 @@ let
   testNames = map (t: { passedTest = t; }) (attrNames tests);
   failed = map (t: {
     failedTest = t.name;
-    expected = toJSON t.expected;
-    result = toJSON t.result;
+    expected = prettyVal t.expected;
+    result = prettyVal t.result;
   }) (lib.debug.runTests tests);
   failedTests = map (f: f.failedTest) failed;
   passed = filter (t: !lib.elem t.passedTest failedTests) testNames;
   result = { inherit passed failed; };
+  prettyVal = let
+    modify = v:
+      let
+        pr = f: {
+          __pretty = f;
+          val = v;
+        };
+      in if lib.isDerivation v then
+        pr (drv: "<δ:${drv.name}:${concatStringsSep "," (attrNames drv)}>")
+      else if [ ] == v then
+        pr (const "[]")
+      else if lib.isList v then
+        pr (l: "[ ${toString (map go l)} ]")
+      else if lib.isAttrs v then
+        pr (a:
+          "{ ${
+            concatStringsSep ", "
+            (lib.attrValues (lib.mapAttrs (n: v: "${n} = ${go v}") v))
+          } }")
+      else
+        v;
+    go = x: lib.generators.toPretty { allowPrettyValues = true; } (modify x);
+  in go;
 in result
