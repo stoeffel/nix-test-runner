c2af3a9e0dc5256e7b7e975f30e9e0267b0fb19b
Stoeffel <schtoeffel@gmail.com>
2020-01-01 17:34:19 +0100

    output file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate clap;
 
 use clap::{value_t, App, Arg};
+use std::error::Error;
+use std::fs::File;
 use std::io::{self, Write};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process;
 use std::time::Instant;
 
@@ -26,10 +28,12 @@ fn main() {
                 .default_value("Human")
                 .possible_values(&nix_test_runner::Reporter::variants())
                 .case_insensitive(true),
+            Arg::from_usage("-o, --output=[FILE] 'Specify output file for test results.'"),
         ])
         .get_matches();
     let reporter = value_t!(matches, "reporter", nix_test_runner::Reporter).unwrap();
     let test_file_path = PathBuf::from(matches.value_of("TEST").unwrap());
+    let output = matches.value_of("output").map(|o| Path::new(o));
     assert!(
         test_file_path.exists(),
         "You need to provide an existing file."
@@ -37,7 +41,21 @@ fn main() {
     match nix_test_runner::run(test_file_path) {
         Ok(result) => {
             let formatted = result.format(now.elapsed(), reporter);
-            io::stdout().write_all(formatted.as_bytes()).unwrap();
+            match output {
+                None => io::stdout().write_all(formatted.as_bytes()).unwrap(),
+                Some(output_path) => {
+                    let display = output_path.display();
+
+                    let mut file = File::create(&output_path).unwrap_or_else(|why| {
+                        panic!("Couldn't create {}: {}", display, why.description())
+                    });
+
+                    match file.write_all(formatted.as_bytes()) {
+                        Err(why) => panic!("Couldn't write to {}: {}", display, why.description()),
+                        Ok(_) => println!("Successfully wrote to {}", display),
+                    }
+                }
+            }
             process::exit(if result.successful() { 0 } else { 1 })
         }
         Err(err) => {
